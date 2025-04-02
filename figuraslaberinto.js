b9d66906a734acfa48b8493bc646b2cd136195a1
abraxas894 <artd_kda@hotmail.com>
2025-04-02 16:07:23 -0600

    Cerati dice: la poesía es lo único real. Remar lo real, Eduardo.

--- figuraslaberinto.js
+++ figuraslaberinto.js
@@ -1,4 +1,34 @@
+const colorHEX = () => {
+  const cadaRGB = () => Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+  const color = `#${cadaRGB()}${cadaRGB()}${cadaRGB()}`;
+  return color;
+};
 
+const invertirColorHEX = (colorFondo) => {
+  // Elimina el "#" si está presente
+  colorFondo = colorFondo.replace('#', '');
+
+  // Convierte el HEX a RGB
+  const r = parseInt(colorFondo.substring(0, 2), 16);
+  const g = parseInt(colorFondo.substring(2, 4), 16);
+  const b = parseInt(colorFondo.substring(4, 6), 16);
+
+  // Calcula el inverso de cada componente
+  const rInverso = 255 - r;
+  const gInverso = 255 - g;
+  const bInverso = 255 - b;
+
+  // Convierte los componentes inversos a HEX y asegura 2 dígitos
+  const hexInverso = `#${rInverso.toString(16).padStart(2, '0')}${gInverso.toString(16).padStart(2, '0')}${bInverso.toString(16).padStart(2, '0')}`;
+
+  return hexInverso;
+}
+
+const colorAleatorio = colorHEX();
+console.log(colorAleatorio);
+
+const colorInverso = invertirColorHEX(colorAleatorio);
+console.log(colorInverso);
 
 const dibujar = () => {
 
@@ -16,10 +46,11 @@ const dibujar = () => {
 
   let trazoAleatorio = Math.floor(Math.random() * 15)    
 
-  let colorAzar = Math.floor(Math.random() * 17);
+  /* let colorAzar = Math.floor(Math.random() * 17);
   let matices = ['aqua', 'pink', 'blue', 'fuchsia', 'Linen', 'green', 'lime', 'salmon', 'violet', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
-  ctx.strokeStyle = matices[colorAzar];    
-
+  ctx.strokeStyle = matices[colorAzar]; */
+  
+  ctx.strokeStyle = colorAleatorio;
   
     let numeroCirculos = Math.floor(Math.random() * 7);
     for (let i = 0; i <= numeroCirculos; i++) {
@@ -29,6 +60,9 @@ const dibujar = () => {
       ctx.stroke();
       }
     }
+
+    let fondoPortada = document.getElementById('soyUnaPortada');
+    fondoPortada.style.backgroundColor = 'colorInverso';
 
 }
 
