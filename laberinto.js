6413eb5a65ac39048f246b3a7b694c7034cb9af6
abraxas894 <artd_kda@hotmail.com>
2025-04-28 15:55:58 -0600

    He agredado algunas funciones durante las vacaciones. He reiterado la misma lógica que había pensado para la segunda columna en las otras dos, sigo experimentando con posibles funcionalidades. Cambié los botones y agregué algunas imágenes. Me gustaría dibujar figuras geométricas aleatorias. Mientras yo me vacío, intento llenar un laberinto que quizá no me corresponda. Debo aceptar el presente y ser valiente. Agradezco que este laberinto haya comenzado cercano a su sonrisa. La soñaré en vida

--- laberinto.js
+++ laberinto.js
@@ -1,148 +1,61 @@
+const primeraColumna = ['amante,', 'festivo y', 'alegre', 'dichoso', 'ofrendas', 'amantes', 'al pecho', 'al cuello', 'te enlacen', 'ufano', 'voluntad', 'aceptas', 'no altivas', 'de ilustres', 'lucidas', 'costosas', 'las cortas', 'víctimas', 'de mi amor,', 'dichosa,', 'y suple,', 'para ti,', 'pido que,', 'escasa y', 'ansioso', 'fino mi', 'felices,', 'unida,', 'eterno', 'eterna,', 'parados', 'suspensos'];
+
+const intermedios = ['caro', 'pronto', 'canta', 'porque', 'finas', 'señas', 'rica', 'dulces', 'firmes,', 'siempre,', 'y ojos', 'solas,', 'sirvan', 'fiestas,', 'danzas,', 'galas', 'muestras de', 'puras de', 'puesto', 'la que,', 'porque', 'visto,', 'cuerdo,', 'corta,', 'quiere', 'amor', 'y yo', 'sola,', 'vive:', 'para que', 'calmen', 'de que'];
+
+const terceraColumna = ['dulce Esposo mío', 'tus felices años', 'sólo mi cariño', 'puede celebrarlos', 'a tu obsequio sean', 'de fino holocausto', 'mi corazón, joya,', 'cadenas mis brazos', 'pues mi amor no ignora', 'que son a tu agrado', 'las mejores joyas', 'las de mis halagos', 'no, en demostraciones', 'de altos aparatos', 'célebres festines', 'de regios saraos', 'el cariño acepta', 'el afecto casto', 'que te ofrezco, Esposa', 'Dueño, te consagro', 'si mi obsequio humilde', 'pareciere acaso', 'no aprecies la ofrenda', 'sino mi cuidado', 'con mi propia vida', 'acrecentar tus años', 'quiero; pero es una', 'la que anima a entreambos', 'vive, y yo en ti viva', 'identificados', 'el Amor y el Tiempo', 'nos miren milagrosos'];
+
+// Se utiliza un algortimo Fisher-Yates para un ordenamiento estocástico
 
 const arrayaleatorio = (array) => {
   for (let i = array.length - 1; i >= 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
-    let k = array[i];
+    let k = array[i]; 
     array[i] = array[j];
     array[j] = k;
   }
   return array;
+  }
+
+// función para definir un rango aleatorio
+
+const rangoAleatorio = (max, min) => {
+  return (Math.floor(Math.random() * (max - min))) + min;
+};
+
+// cambiarPrimera() modifica el orden de los versos de la primera columna
+
+const cambiarPrimera = () => {
+  let nuevaPrimera = arrayaleatorio(primeraColumna);
+  for (let i = 1; i <= 32; i++) {
+    document.getElementById(`v${i}c1`).innerHTML = nuevaPrimera[i];
+  }
+  for (let j = 1; j <= 32; j = j + 4) {
+    document.getElementById(`v${j}c1`).style.color = '#8b0000';
+    document.getElementById(`v${j}c1`).style.textTransform = 'uppercase';
+  }
 }
 
-let intermedios = ['caro', 'pronto', 'canta', 'porque', 'finas', 'señas', 'rica', 'dulces', 'firmes,', 'siempre,', 'y ojos', 'solas,', 'sirvan', 'fiestas,', 'danzas,', 'galas', 'muestras de', 'puras de', 'puesto', 'la que,', 'porque', 'visto,', 'cuerdo,', 'corta,', 'quiere', 'amor', 'y yo', 'sola,', 'vive:', 'para que', 'calmen', 'de que'];
-
-let laberinto = `Amante,—${intermedios[0]},—dulce Esposo mío,
-festivo y—${intermedios[1]}—tus felices años
-alegre—${intermedios[2]}—sólo mi cariño,
-dichoso—${intermedios[3]}—puede celebrarlos.
-
-Ofrendas—${intermedios[4]}—a tu obsequio sean
-amantes—${intermedios[5]}—de fino holocausto,
-al pecho—${intermedios[6]}—mi corazón, joya,
-al cuello—${intermedios[7]}—cadenas mis brazos.
-
-Te enlacen—${intermedios[8]}—pues mi amor no ignora
-ufano—${intermedios[9]}—que son a tu agrado
-voluntad—${intermedios[10]}—las mejores joyas,
-aceptas—${intermedios[11]}—las de mis halagos.
-
-No altivas—${intermedios[12]}—,no, en demostraciones
-de ilustres—${intermedios[13]}—de altos aparatos,
-lucidas—${intermedios[14]}—célebres festines,
-costosas—${intermedios[15]}—de regios saraos.
-
-Las cortas—${intermedios[16]}—el cariño acepta,
-víctimas—${intermedios[17]}—el afecto casto
-de mi amor,—${intermedios[18]}—que te ofrezco, Esposa
-dichosa,—${intermedios[19]}—Dueño, te consagro.
-
-Y suple,—${intermedios[20]}—si mi obsequio humilde
-para ti,—${intermedios[21]}— pareciere acaso,
-pido que,—${intermedios[22]}—no aprecies la ofrenda
-escasa y—${intermedios[23]}—sino mi cuidado.
-
-Ansioso—${intermedios[24]}—con mi propia vida
-fino mi—${intermedios[25]}—acrecentar tus años
-felices,—${intermedios[26]}—quiero; pero es una,
-unida,—${intermedios[27]}—la que anima a entreambos.
-
-Eterno—${intermedios[28]}—vive, y yo en ti viva
-eterna,—${intermedios[29]}— identificados,
-parados—${intermedios[30]}—el Amor y el Tiempo
-suspensos—${intermedios[31]}— nos miren milagrosos.`;
-
+// cambiarIntermedios() representa la principal función de esta pieza
 
 const cambiarIntermedios = () => {
-  arrayaleatorio(intermedios);
-  console.log(intermedios);
-  let laberinto = `Amante,—${intermedios[0]},—dulce Esposo mío,
-  festivo y—${intermedios[1]}—tus felices años
-  alegre—${intermedios[2]}—sólo mi cariño,
-  dichoso—${intermedios[3]}—puede celebrarlos.
-
-  Ofrendas—${intermedios[4]}—a tu obsequio sean
-  amantes—${intermedios[5]}—de fino holocausto,
-  al pecho—${intermedios[6]}—mi corazón, joya,
-  al cuello—${intermedios[7]}—cadenas mis brazos.
-
-  Te enlacen—${intermedios[8]}—pues mi amor no ignora
-  ufano—${intermedios[9]}—que son a tu agrado
-  voluntad—${intermedios[10]}—las mejores joyas,
-  aceptas—${intermedios[11]}—las de mis halagos.
-
-  No altivas—${intermedios[12]}—,no, en demostraciones
-  de ilustres—${intermedios[13]}—de altos aparatos,
-  lucidas—${intermedios[14]}—célebres festines,
-  costosas—${intermedios[15]}—de regios saraos.
-
-  Las cortas—${intermedios[16]}—el cariño acepta,
-  víctimas—${intermedios[17]}—el afecto casto
-  de mi amor,—${intermedios[18]}—que te ofrezco, Esposa
-  dichosa,—${intermedios[19]}—Dueño, te consagro.
-
-  Y suple,—${intermedios[20]}—si mi obsequio humilde
-  para ti,—${intermedios[21]}— pareciere acaso,
-  pido que,—${intermedios[22]}—no aprecies la ofrenda
-  escasa y—${intermedios[23]}—sino mi cuidado.
-
-  Ansioso—${intermedios[24]}—con mi propia vida
-  fino mi—${intermedios[25]}—acrecentar tus años
-  felices,—${intermedios[26]}—quiero; pero es una,
-  unida,—${intermedios[27]}—la que anima a entreambos.
-
-  Eterno—${intermedios[28]}—vive, y yo en ti viva
-  eterna,—${intermedios[29]}— identificados,
-  parados—${intermedios[30]}—el Amor y el Tiempo
-  suspensos—${intermedios[31]}— nos miren milagrosos.`
-
-    return laberinto;
+  let celda = arrayaleatorio(intermedios);
+  for (let i = 1;  i <= 32; i++) {
+    document.getElementById(`verso${i}`).innerHTML = celda[i];
+  }
 }
 
-console.log(cambiarIntermedios());
+// cambiarTercera() modifca el orden de los versos de la última columna
 
-/* let celda = arrayaleatorio(intermedios);
+const cambiarTercera = () => {
+  let nuevaTercera = arrayaleatorio(terceraColumna);
+  for (let i = 1; i <= 32; i++) {
+    document.getElementById(`v${i}c3`).innerHTML = nuevaTercera[i];
+  }
+    
+}
 
-const laberintoEndecasilabo = `Amante,—${celda[0]},—dulce Esposo mío,
-festivo y—${celda[1]}—tus felices años
-alegre—${celda[2]}—sólo mi cariño,
-dichoso—${celda[3]}—puede celebrarlos.
 
-Ofrendas—${celda[4]}—a tu obsequio sean
-amantes—${celda[5]}—de fino holocausto,
-al pecho—${celda[6]}—mi corazón, joya,
-al cuello—${celda[7]}—cadenas mis brazos.
 
-Te enlacen—${celda[8]}—pues mi amor no ignora
-ufano—${celda[9]}—que son a tu agrado
-voluntad—${celda[10]}—las mejores joyas,
-aceptas—${celda[11]}—las de mis halagos.
 
-No altivas—${celda[12]}—,no, en demostraciones
-de ilustres—${celda[13]}—de altos aparatos,
-lucidas—${celda[14]}—célebres festines,
-costosas—${celda[15]}—de regios saraos.
 
-Las cortas—${celda[16]}—el cariño acepta,
-víctimas—${celda[17]}—el afecto casto
-de mi amor,—${celda[18]}—que te ofrezco, Esposa
-dichosa,—${celda[19]}—Dueño, te consagro.
 
-Y suple,—${celda[20]}—si mi obsequio humilde
-para ti,—${celda[21]}— pareciere acaso,
-pido que,—${celda[22]}—no aprecies la ofrenda
-escasa y—${celda[23]}—sino mi cuidado.
-
-Ansioso—${celda[24]}—con mi propia vida
-fino mi—${celda[25]}—acrecentar tus años
-felices,—${celda[26]}—quiero; pero es una,
-unida,—${celda[27]}—la que anima a entreambos.
-
-Eterno—${celda[28]}—vive, y yo en ti viva
-eterna,—${celda[29]}— identificados,
-parados—${celda[30]}—el Amor y el Tiempo
-suspensos—${celda[31]}— nos miren milagrosos.`
-
-console.log(laberinto);
-console.log(celda);
-console.log(laberintoEndecasilabo);
-*/
